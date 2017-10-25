bb0196f2742a61aa153b11ab364252e7c35fd3bc
Md Abir Hasan Zoha <abirhasanzoha@gmail.com>
2017-10-26 02:09:51 +0600

    addedd addProduct view

---
+++ src/add_product/AddProductController.java
@@ -0,0 +1,44 @@
+package add_product;
+
+import data_helper.DataHelper;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class AddProductController {
+    @FXML
+    TextField nametxt, codetxt, cratetxt, hratetxt, companytxt;
+    @FXML
+    Button addbtn;
+
+    public void initialize(){
+
+
+        addbtn.disableProperty().bind(
+                Bindings.isEmpty(nametxt.textProperty())
+                .or(Bindings.isEmpty(codetxt.textProperty()))
+                .or(Bindings.isEmpty(cratetxt.textProperty()))
+                .or(Bindings.isEmpty(hratetxt.textProperty()))
+                .or(Bindings.isEmpty(companytxt.textProperty()))
+        );
+
+        addbtn.setOnAction(event -> {
+            Double cRate, hRate;
+            cRate = Double.parseDouble(cratetxt.getText());
+            hRate = Double.parseDouble(hratetxt.getText());
+
+            boolean data = DataHelper.insertProduct(nametxt.getText(), codetxt.getText(), cRate, hRate, companytxt.getText());
+            if(data){
+                nametxt.clear();
+                codetxt.clear();
+                hratetxt.clear();
+                cratetxt.clear();
+                companytxt.clear();
+            }
+            System.out.println("Product Data inserted: "+ data);
+
+        });
+    }
+}
